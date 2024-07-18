653fa6ece47c5ff40af2f69323a9ffcd735753e0
David Agbaniyaka <fiyin.agba@gmail.com>
2024-07-18 18:49:21 -0400

    Changes

---
+++ app/mazegame/page.tsx
@@ -0,0 +1,374 @@
+import { buttonVariants } from '@/components/ui/button'
+import { siteConfig } from '@/config/site'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+      <header className="bg-white">
+        <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+
+          <div className="flex flex-1 items-center justify-end md:justify-between">
+            <nav aria-label="Global" className="hidden md:block">
+              <ul className="flex items-center gap-6 text-sm">
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">Intro</a>
+                </li>
+
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">Features</a>
+                </li>
+
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">About</a>
+                </li>
+                <li>
+                  <a className="text-gray-500 transition hover:text-gray-500/75" href="#">Optional</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+          <div className="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full">
+            <img
+              alt=""
+              src="/mazegame.png"
+              className="absolute inset-0 h-full w-full object-cover"
+            />
+          </div>
+
+          <div className="lg:py-24">
+            <h2 className="text-3xl font-bold sm:text-4xl">Maze Game</h2>
+
+            <p className="mt-4 text-gray-600">
+              Explore a dynamic 3D maze with textured walls in this immersive raycasting game!
+            </p>
+
+            <a
+              href="https://github.com/agbaniongithub/maze-game"
+              className="mt-8 inline-block rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+            >
+              View The Deployed Project
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <section className="bg-gray-900 text-white">
+        <div className="max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
+          <div className="max-w-xl">
+            <h2 className="text-3xl font-bold sm:text-4xl">Features</h2>
+
+            <p className="mt-4 text-gray-300">
+              Here are the features I implemented in the game as required in the task.
+            </p>
+          </div>
+
+          <div className="mt-8 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path
+                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                  ></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Task 0: Walls</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  I created a window with SDL2.
+                  I used raycasting to draw walls on my window.
+                  The color of the walls are different from the color of the ground/ceiling.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path
+                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                  ></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Task 1: Orientation.</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  Walls facing NORTH and SOUTH are in a different color as walls facing EAST and WEST, making it look 3 dimentional.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path
+                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                  ></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">-Task 2: Rotations</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  I implemented logic to make the camera move left and right, based on the mouse movement.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path
+                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                  ></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">-Task 3: Move</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  I provided a way to move the player/camera up, down, left and right with the w, s, a, d keys on the keyboard.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path
+                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                  ></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Task 4: Ouch!</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  I made sure the player is unable to enter walls.
+                </p>
+              </div>
+            </div>
+
+            <div className="flex items-start gap-4">
+              <span className="shrink-0 rounded-lg bg-gray-800 p-4">
+                <svg
+                  className="h-5 w-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path
+                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+                  ></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <div>
+                <h2 className="text-lg font-bold">Task 5: Parser</h2>
+
+                <p className="mt-1 text-sm text-gray-300">
+                  I made sure my map array is coming from a different file.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+          <div className="max-w-3xl">
+            <h2 className="text-3xl font-bold sm:text-4xl">
+              About
+            </h2>
+          </div>
+
+          <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+            <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+              <img
+                alt="Maze Game"
+                src="/mazegame.png"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+
+            <div className="lg:py-16">
+              <article className="space-y-4 text-gray-600">
+                <p>
+                  My inspiration for doing this project is my passion for gaming. I wanted to see what I could build and I am glad that I have been able to execute it and learn about technologies like SDL2 and Raycasting.
+                </p>
+
+
+              </article>
+            </div>
+            <div className="flex flex-col gap-4 justify-center md:flex-row">
+            <Link
+              href="/projects"
+              className={cn(buttonVariants({ size: "lg" }), "w-full md:w-fit")}
+            >
+              View my projects
+            </Link>
+            <Link
+              href="/blog"
+              className={cn(buttonVariants({ size: "lg" }), "w-full md:w-fit")}
+            >
+              View my blog
+            </Link>
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                buttonVariants({ variant: "outline", size: "lg" }),
+                "w-full md:w-fit"
+              )}
+            >
+              GitHub
+            </Link>
+            <Link
+              href={siteConfig.links.linkedin}
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                buttonVariants({ variant: "outline", size: "lg" }),
+                "w-full md:w-fit"
+              )}
+            >
+              LinkedIn
+            </Link>
+            <Link
+              href={siteConfig.links.linkedin}
+              target="_blank"
+              rel="noreferrer"
+              className={cn(
+                buttonVariants({ variant: "outline", size: "lg" }),
+                "w-full md:w-fit"
+              )}
+            >
+              CV
+            </Link>
+          </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
+          <div className="max-w-3xl">
+            <h2 className="text-3xl font-bold sm:text-4xl">
+              Optional Video
+            </h2>
+          </div>
+
+          <div className="mt-8 grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+            <div className="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+              <img
+                alt="Maze Game"
+                src="/mazegame.png"
+                className="absolute inset-0 h-full w-full object-cover"
+              />
+            </div>
+
+            <div className="lg:py-16">
+              <article className="space-y-4 text-gray-600">
+                <p>
+                  Hi, My name is David Agbaniyaka. I worked solo on this project. 
+                </p>
+
+
+              </article>
+            </div>
+          </div>
+        </div>
+      </section>
+    </section>
+  )
+}
+
+export default page
--- config/site.ts
+++ config/site.ts
@@ -18,41 +18,51 @@ export const siteConfig = {
     siteUrl: "https://fancycounterbyagbani.netlify.app"
   },
 
-  projectsArray:  [{
-    title: "Space Tourism Website",
-    description:
-    "A challenging UI for a space exploration website. I was the FrontEnd Developer",
-    tags: ["HTML", "Tailwind CSS", "Next.JS"],
-    imageUrl: "/space-tourism.png",
-    githubUrl: "https://github.com/agbaniongithub/space-tourism-website",
-    siteUrl: "space-tourism-website.vercel.app"
-  },
-  {
-    title: "Word Analytics",
-    description:
-    "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["HTML", "CSS", "Javascript"],
-    imageUrl: "/wordanalytics.png",
-    githubUrl: "https://github.com/agbaniongithub/Word-Analytics",
-    siteUrl: "https://wordanalyticsbyagbani.netlify.app"
-  },
-  {
-    imageUrl: "/fancy-counter.png",
-    title: "Fancy Counter",
-    description: "Fancy Counter is a simple yet powerful and beautiful counter application for the web.",
-    tags: ["HTML", "CSS", "Javascript"],
-    githubUrl: "https://github.com/agbaniongithub/fancy-counter",
-    siteUrl: "https://fancycounterbyagbani.netlify.app"
-  },
-  {
-    title: "CorpComment",
-    description:
-      "Users can give public feedback to companies.",
-    tags: ["HMTL", "CSS", "Javascript", "Node.JS"],
-    imageUrl: "/corpcomment.png",
-    githubUrl: "https://github.com/agbaniongithub/corpcomment",
-    siteUrl: "https://corpcommentbyagbani.netlify.app"
-  },
+  projectsArray: [
+    {
+      title: "Maze Game",
+      description:
+        "A challenging UI for a space exploration website. I was the FrontEnd Developer",
+      tags: ["SDL2", "Ray Casting", "C"],
+      imageUrl: "/space-tourism.png",
+      githubUrl: "https://github.com/agbaniongithub/space-tourism-website",
+      siteUrl: "/mazegame"
+    },
+    {
+      title: "Space Tourism Website",
+      description:
+        "A challenging UI for a space exploration website. I was the FrontEnd Developer",
+      tags: ["HTML", "Tailwind CSS", "Next.JS"],
+      imageUrl: "/space-tourism.png",
+      githubUrl: "https://github.com/agbaniongithub/space-tourism-website",
+      siteUrl: "space-tourism-website.vercel.app"
+    },
+    {
+      title: "Word Analytics",
+      description:
+        "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+      tags: ["HTML", "CSS", "Javascript"],
+      imageUrl: "/wordanalytics.png",
+      githubUrl: "https://github.com/agbaniongithub/Word-Analytics",
+      siteUrl: "https://wordanalyticsbyagbani.netlify.app"
+    },
+    {
+      imageUrl: "/fancy-counter.png",
+      title: "Fancy Counter",
+      description: "Fancy Counter is a simple yet powerful and beautiful counter application for the web.",
+      tags: ["HTML", "CSS", "Javascript"],
+      githubUrl: "https://github.com/agbaniongithub/fancy-counter",
+      siteUrl: "https://fancycounterbyagbani.netlify.app"
+    },
+    {
+      title: "CorpComment",
+      description:
+        "Users can give public feedback to companies.",
+      tags: ["HMTL", "CSS", "Javascript", "Node.JS"],
+      imageUrl: "/corpcomment.png",
+      githubUrl: "https://github.com/agbaniongithub/corpcomment",
+      siteUrl: "https://corpcommentbyagbani.netlify.app"
+    },
   ]
 };
 export type SiteConfig = typeof siteConfig;
